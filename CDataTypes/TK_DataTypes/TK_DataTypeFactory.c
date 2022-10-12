ec670b650c5d6bf4c2b72d5fd2c06c5d32633a51
TheTimeee <lk20031967@gmail.com>
2022-10-12 19:48:10 +0200

    Test2

--- CDataTypes/TK_DataTypes/TK_DataTypeFactory.c
+++ CDataTypes/TK_DataTypes/TK_DataTypeFactory.c
@@ -7,9 +7,6 @@ bool TK_FactoryCreate(void* obj, enum TK_DataTypes type)
 		case TK_STRING:
 			return TK_String_Create(obj);
 			break;
-		case 2:
-			printf("Lol");
-			break;
 		default:
 			return false;
 			break;
